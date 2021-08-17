060ac86d1c081e0218c3fa0954a9585bd0e98742
faradeycage <smeernova@gmail.com>
2021-08-17 23:36:30 +0300

    исправила проверку на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,7 @@
 function checkSpam(str) {
   // ваш код...
   //true, если str содержит '1xBet' или 'XXX', а иначе false
-  lowCaseString = str.toLowerCase();
+  let lowCaseString = str.toLowerCase();
   if (lowCaseString.includes("xxx") || lowCaseString.includes("1xbet")) {
     return true;
   }
