a626183571ee82e46be3b763a7fcc1716c2ef951
faradeycage <smeernova@gmail.com>
2021-08-20 00:57:04 +0300

    суммирование зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,21 @@
+const salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+  for (let k in salaries) {
+    if (typeof salaries[k] == "number" && !isNaN(salaries[k]) && isFinite(salaries[k])) {
+      sum = sum + salaries[k];
+    }
+  }
+  return sum;
 }
+
+sumSalary(salaries);
