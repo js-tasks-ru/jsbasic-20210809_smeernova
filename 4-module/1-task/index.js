0d4ddf66d409da4ded012161616f25c89202771a
faradeycage <smeernova@gmail.com>
2021-08-30 23:44:13 +0300

    1 задача 4 модуля

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,26 @@
+let friends = [
+  {
+    firstName: 'Artsiom',
+    lastName: 'Mezin'
+  },
+  {
+    firstName: 'Ilia',
+    lastName: 'Kantor'
+  },
+  {
+    firstName: 'Christopher',
+    lastName: 'Michael'
+  }
+];
+
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement('ul');
+  for (i = 0; i < friends.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = friends[i].firstName + '\u0020' + friends[i].lastName;
+    ul.appendChild(li);
+  }
+  return ul;
 }
+makeFriendsList(friends);
