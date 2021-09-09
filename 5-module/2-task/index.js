db5a0c832cddd7a83e3fe60b07aa9a10a10c58bb
faradeycage <smeernova@gmail.com>
2021-09-09 16:37:13 +0300

    5 модуль 2 задача

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
   // ваш код...
+  document.querySelector('.toggle-text-button').addEventListener("click", () => {
+
+    if (document.querySelector('#text').hasAttribute("hidden")) {
+      document.querySelector('#text').removeAttribute("hidden");
+      console.log(document.querySelector('#text'));
+    }
+    else{
+      document.querySelector('#text').setAttribute("hidden", true);
+    }
+
+  });
 }
+
