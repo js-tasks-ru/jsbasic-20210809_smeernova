5ff9543b59d5075ca71860ecc9c7b71b09c4714b
faradeycage <smeernova@gmail.com>
2021-09-09 13:56:30 +0300

    4 модуль 3 задача + 5 модуль 1 задача

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,36 @@
-function highlight(table) {
-  // ваш код...
-}
+
+  function highlight(table) {
+    // ваш код...
+
+    let myTable = document.querySelector('tbody');
+    let strings = myTable.querySelectorAll('tr');
+
+    for (let currentString of strings) {
+      let statusCell = currentString.lastElementChild;
+      if (statusCell.hasAttribute('data-available')) {
+        if (statusCell.getAttribute('data-available') === 'true') {
+          currentString.classList.add('available');
+        }
+        else {
+          currentString.classList.add('unavailable');
+        }
+      }
+      else {
+        currentString.setAttribute('hidden', true);
+      }
+
+      let genderCell = statusCell.previousElementSibling;
+      if (genderCell.textContent == 'm') {
+        currentString.classList.add('male');
+      }
+      if (genderCell.textContent == 'f') {
+        currentString.classList.add('female');
+      }
+
+      let ageCell = genderCell.previousElementSibling;
+      if (+ageCell.textContent < 18) {
+        currentString.style = "text-decoration: line-through";
+      }
+    }
+  }
+
